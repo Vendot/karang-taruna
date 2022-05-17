86dbb3be6eadc5b78218ac582e5cff1110086ee7
Vendot <43804249+Vendot@users.noreply.github.com>
2022-05-17 20:47:21 +0700

    Add files via upload

---
+++ karangtaruna/profile_katar/top_wrapper.php
@@ -0,0 +1,5 @@
+<div class="visi-top-wrapper">
+    <div class="container">
+        <img src="images/logo.png"/>
+    </div>
+</div>
---
+++ karangtaruna/profile_user/info_pribadi.php
@@ -0,0 +1,21 @@
+<?php
+
+    include_once("./index.php");
+
+?>
+
+<div class="my-profile">
+
+    <?php
+
+        include_once("profile_user/my_profile.php");
+
+    ?>
+
+    <div class="list-profile-edit">
+        <div class="container">
+            <p>testes</p>
+            <p>dfdfdfdfdf</p>
+        </div>
+    </div>
+</div>
